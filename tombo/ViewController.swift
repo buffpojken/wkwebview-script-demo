e2cb59bdcb8437db6f80233e1e40704ebed30532
buffpojken <daniel@sykewarrior.com>
2017-01-06 09:59:04 +0100

    meepmeep

--- tombo/ViewController.swift
+++ tombo/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKScriptMessageHandler {
 
+    var webView: WKWebView?
+    
+    override func loadView() {
+        super.loadView()
+        let content = WKUserContentController()
+        content.add(self, name: "mungo")
+        let config = WKWebViewConfiguration()
+        config.userContentController = content;
+        
+        webView = WKWebView(frame: self.view.bounds, configuration: config)
+        self.view = webView!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let url = Bundle.main.url(forResource: "index", withExtension: "html"){
+            webView!.loadFileURL(url, allowingReadAccessTo: url)
+        }
     }
-
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        switch(message.name){
+            case "mungo":
+                print("This was called");
+                print(message.body)        
+            default:
+                    print("unknown message called...");
+            
+        }
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake{
+            webView!.evaluateJavaScript("nativeCall()", completionHandler: nil)
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
